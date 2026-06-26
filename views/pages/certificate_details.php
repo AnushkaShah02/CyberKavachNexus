4492ef436cfab25f42ae977beecfbc9d7765268c
Anushka Shah <25cs097@charusat.edu.in>
2026-06-26 16:11:43 +0530

    Fix certificate generation and local database configuration

--- views/pages/certificate_details.php
+++ views/pages/certificate_details.php
@@ -14,6 +14,9 @@ use CyberKavach\Nexus\Helpers\MailHelper;
 
 require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2));
+$dotenv->safeLoad();
+
 AuthMiddleware::handle();
 
 $db = Database::getConnection();
